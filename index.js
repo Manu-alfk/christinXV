76ed0739aa721fb3da1172610b7e3922622cea29
Manu-alfk <73673602+Manu-alfk@users.noreply.github.com>
2022-09-06 21:37:35 -0600

    sonido

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 var bu_play = document.querySelector(".bu_play");
 var inicio_oculto = document.querySelector(".inicio_oculto");
 var sobre = document.querySelector(".sobre");
+var rasgado = document.querySelector(".rasgado");
+var papel_de_carta = document.querySelector(".papel_de_carta");
+
+var papel_sonido = document.getElementById("rasgado_audio"); 
 
 
 bu_play.addEventListener("click", function () {  
@@ -12,7 +16,11 @@ bu_play.addEventListener("click", function () {
 })
 
 sobre.addEventListener("click", function () {
+    papel_sonido.play();
     console.log("sobre");
-    sobre.classList.toggle("stop_sobre"); 
+    sobre.classList.toggle ("stop_sobre");
+    papel_de_carta.classList.toggle ("stop_sobre");
+    //sobre.className =  "stop_sobre";
+    rasgado.style.animation = "rasgar_sobre 2s";
 })
 
